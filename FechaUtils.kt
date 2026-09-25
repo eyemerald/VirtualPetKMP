5d8afc2263a41b2d7c20b28f3a44a8154914f02d
enriquerobles <eyemerald@gmail.com>
2026-09-25 06:16:50 +0200

    Limpieza: eliminar archivos huérfanos en raíz

--- FechaUtils.kt
+++
@@ -1,5 +0,0 @@
-fun LocalDate.toFormatoEuropeo(): String {
-    val dia = dayOfMonth.toString().padStart(2, '0')
-    val mes = monthNumber.toString().padStart(2, '0')
-    return "$dia/$mes/$year"
-}
